aac0bc3f80d945cd2b37f8989c8d936514114b18
Jason Valenzuela <jvalenzuela1977@gmail.com>
2024-06-03 16:57:02 -0400

    Implement PDF paragraph splitting.

---
+++ testgen/pdf.py
@@ -0,0 +1,31 @@
+# This module implements generating PDF output.
+
+
+def split_paragraphs(s):
+    """Separates a string into a list of paragraphs.
+
+    This function is used to convert a string possibly containing multiple
+    paragraphs delimited by empty lines into separate strings, one per
+    paragraph, which can then be used by ReportLab Paragraph instances.
+    """
+    # Assembly buffer to hold lines for each paragraph.
+    plines = [
+        [] # Inner lists contain lines for a single paragraph.
+        # [] Additional inner lists for each additional paragraph.
+    ]
+
+    for line in s.splitlines():
+        stripped = line.strip()
+
+        # Non-blank lines are appended to the current paragraph's line list.
+        if stripped:
+            plines[-1].append(stripped)
+
+        # A blank line indicates two or more consecutive newlines, possibly
+        # with intervening whitespace, so begin a new line list for a new
+        # paragraph.
+        else:
+            plines.append([])
+
+    # Assemble each non-empty inner list into a paragraph.
+    return [' '.join(lines) for lines in plines if lines]
