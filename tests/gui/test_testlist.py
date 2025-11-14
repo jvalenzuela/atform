jvalenzuela/atform

commit 8f2c83e1375a0387e272a72bc9c954a5cf8f2f01

Remove unnecessary section ID filter from GUI preview.
The test list widget already ensures sections are not sent for preview,
making the preview widget filter redundant.

tests/gui/igtest.py
@@ -121,8 +121,11 @@ class TestList(InteractiveGuiTestCase):
         This is a manual test because the preview action is bound to
         Treeview tags, which are not easily simulated.
         """
+        atform.set_id_depth(2)
         atform.add_test("title")
-        self.start_gui()
+        self.start_gui(
+            instruction="Click the test and ensure it is displayed in the Preview window, and click the parent section and ensure it does not affect the Preview window.",
+        )
 
     @patch("atform.gui.preview.show")
     def test_multiple_select(self, *_mocks):

atform/gui/preview.py
@@ -72,12 +72,7 @@ class Preview(tkwidget.LabelFrame):  # pylint: disable=too-many-ancestors
 
     def show(self, tid):
         """Diplays test content for a given ID."""
-        # Ignore section IDs.
-        try:
-            test = state.tests[tid]
-        except KeyError:
-            return
-
+        test = state.tests[tid]
         self.title.set(test.full_name)
         self.location.show(test)
 

commit eb8237b80ecf1db38bf3112a724812ae7dae2d73

Fix exception when simultaneously selecting tests and sections.
This problem was inadvertenly caused by 8f2c83e due to unepxected return
values from Tkinter Treeview.focus() when multiple items are selected.
The original preview code was not reverted in order to facilitate
easier unit testing, i.e., tests are constrained to the testlist module.

atform/gui/testlist.py
@@ -112,7 +112,14 @@ class TestList(tkwidget.Frame):  # pylint: disable=too-many-ancestors
     def _preview(self, _event):
         """Event handler for clicks on a test item to dispatch a preview."""
         tid = self.tree.ttv_focus()
-        preview.show(tid)
+
+        # Even though this callback is bound to test items only, it is
+        # possible for Treeview.focus() to return a section or subsection
+        # when simultaneously selecting a section and a test. The preview
+        # dispatch is therefore filtered for test IDs only to prevent
+        # non-test IDs from being previewed.
+        if len(tid) == len(state.current_id):
+            preview.show(tid)
 
     @property
     def selected_tests(self):

tests/gui/igtest.py
@@ -115,18 +115,6 @@ class TestList(InteractiveGuiTestCase):
             instruction="Verify selected count is equal to 42.",
         )
 
-    def test_preview(self):
-        """Confirm a test is displayed in the Preview window when clicked on in the list of tests.
-
-        This is a manual test because the preview action is bound to
-        Treeview tags, which are not easily simulated.
-        """
-        atform.set_id_depth(2)
-        atform.add_test("title")
-        self.start_gui(
-            instruction="Click the test and ensure it is displayed in the Preview window, and click the parent section and ensure it does not affect the Preview window.",
-        )
-
     @patch("atform.gui.preview.show")
     def test_multiple_select(self, *_mocks):
         """Confirm multiple items can be simultaneously selected."""

tests/gui/test_testlist.py
@@ -2,13 +2,60 @@
 
 import tkinter as tk
 import unittest
+from unittest.mock import patch
 
 import atform
 from .. import utils
 
 
+@patch("atform.gui.testlist.TupleTreeview.focus")
+@patch("atform.gui.preview.show")
+class Preview(unittest.TestCase):
+    """Tests for sending tests for preview when clicked."""
+
+    def setUp(self):
+        utils.reset()
+        self.tl = atform.gui.testlist.TestList(None)
+        atform.set_id_depth(3)
+        atform.add_test("foo")
+        self.id = utils.get_test_content().id
+        self.tl.add_test(self.id)
+
+    def test_click_test(self, mock_show, mock_focus):
+        """Confirm preview when a test item is clicked."""
+        mock_focus.return_value = str(self.id)
+        self.click_item(self.id)
+        mock_show.assert_called_once()
+
+    def test_section_focus(self, mock_show, mock_focus):
+        """Confirm no preview when clicking a test but focus() returns a section.
+
+        See testlist.TestList._preview() comment for details why this
+        unit test exists.
+        """
+        mock_focus.return_value = str(self.id[:-1])
+        self.click_item(self.id)
+        mock_show.assert_not_called()
+
+    def test_click_section(self, mock_show, mock_focus):
+        """Confirm no preview when a section item is clicked."""
+        target = self.id[:-1]
+        mock_focus.return_value = str(target)
+        self.click_item(target)
+        mock_show.assert_not_called()
+
+    def click_item(self, item):
+        """Simulates clicking on a given tree item."""
+        self.tl.tree.ttv_see(item)  # Target must be visible to be clicked.
+        x, y, w, h = self.tl.tree.bbox(str(item))
+        click_x = x + (w // 2)
+        click_y = y + (h // 2)
+        self.tl.tree.event_generate("<Button-1>", x=click_x, y=click_y)
+        self.tl.tree.event_generate("<ButtonRelease-1>", x=click_x, y=click_y)
+
+
 class Base(unittest.TestCase):
-    """Base class for all test cases."""
+    """Base class for various test cases."""
 
     def setUp(self):
         utils.reset()