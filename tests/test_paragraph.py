0883f60dc69374f656249908d5d241cad3c9a623
Jason Valenzuela <jvalenzuela1977@gmail.com>
2026-09-02 12:33:53 -0400

    Change method names to lower-case.
    Resolves Pylint invalid-name(C0103) messages.

--- tests/test_paragraph.py
+++ tests/test_paragraph.py
@@ -9,51 +9,51 @@ import atform
 class SplitParagraphs(unittest.TestCase):
     """Unit tests for the split_paragraphs() function."""
 
-    def test_single_LF(self):
+    def test_single_lf(self):
         """Confirm lines separated by a single newline are not split."""
         self.assert_result("foo bar\nspam eggs", ["foo bar spam eggs"])
 
-    def test_single_CRLF(self):
+    def test_single_crlf(self):
         """Confirm lines separated by a single CRLF are not split."""
         self.assert_result("foo bar\r\nspam eggs", ["foo bar spam eggs"])
 
-    def test_two_LF(self):
+    def test_two_lf(self):
         """Confirm lines separated by two newlines are split."""
         self.assert_result("foo bar\n\nspam eggs", ["foo bar", "spam eggs"])
 
-    def test_two_CRLF(self):
+    def test_two_crlf(self):
         """Confirm lines separated by two CRLF are split."""
         self.assert_result("foo bar\r\n\r\nspam eggs", ["foo bar", "spam eggs"])
 
-    def test_multiple_LF(self):
+    def test_multiple_lf(self):
         """Confirm lines separated by more than two newlines are split."""
         self.assert_result("foo bar\n\n\nspam eggs", ["foo bar", "spam eggs"])
 
-    def test_multiple_CRLF(self):
+    def test_multiple_crlf(self):
         """Confirm lines separated by more than two CRLFs are split."""
         self.assert_result("foo bar\r\n\r\n\r\nspam eggs", ["foo bar", "spam eggs"])
 
-    def test_leading_LF(self):
+    def test_leading_lf(self):
         """Confirm leading newlines do not create blank paragraphs."""
         self.assert_result("\n\nfoo bar", ["foo bar"])
 
-    def test_leading_CRLF(self):
+    def test_leading_crlf(self):
         """Confirm leading CRLFs do not create blank paragraphs."""
         self.assert_result("\r\n\r\nfoo bar", ["foo bar"])
 
-    def test_trailing_LF(self):
+    def test_trailing_lf(self):
         """Confirm trailing newlines do not create blank paragraphs."""
         self.assert_result("foo bar\n\n", ["foo bar"])
 
-    def test_trailing_CRLF(self):
+    def test_trailing_crlf(self):
         """Confirm trailing CRLFs do not create blank paragraphs."""
         self.assert_result("foo bar\r\n\r\n", ["foo bar"])
 
-    def test_whitespace_between_LF(self):
+    def test_whitespace_between_lf(self):
         """Confirm intervening whitespace between newlines is ignored."""
         self.assert_result("foo bar\n \t\nspam eggs", ["foo bar", "spam eggs"])
 
-    def test_whitespace_between_CRLF(self):
+    def test_whitespace_between_crlf(self):
         """Confirm intervening whitespace between CRLFs is ignored."""
         self.assert_result("foo bar\r\n \t\r\nspam eggs", ["foo bar", "spam eggs"])
 
