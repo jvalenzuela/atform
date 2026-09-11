jvalenzuela/atform

commit 0f386653fef879eceda043b27ec2aa68429e2dd0

Add option to list empty reference categories in PDF output.

atform/ref.py
@@ -4,11 +4,28 @@ This module implements handling for external references, i.e., setup for
 content passed to the add_test() references parameter.
 """
 
+import collections
+
 from . import addtest
 from . import error
 from . import id as id_
 from . import misc
-from . import state
+
+
+# Stores a single category as defined by add_reference_category().
+Category = collections.namedtuple(
+    "Category",
+    ["title", "persist"],
+)
+
+
+# Defined reference categories, keyed by label. Stored as an ordered
+# dictionary because the order the categories are created defines
+# the order they are listed in the output documents.
+#
+# This attribute must only be accessed externally by importing the entire
+# module; see the state module for details.
+categories: dict[str, Category] = collections.OrderedDict()
 
 
 ################################################################################
@@ -20,7 +37,7 @@ from . import state
 
 @error.exit_on_script_error
 @misc.setup_only
-def add_reference_category(title, label):
+def add_reference_category(title, label, *, persist=False):
     """Creates a topic for listing external references.
 
     This function does not create any actual references; they must be
@@ -36,19 +53,30 @@ def add_reference_category(title, label):
         label (str): A shorthand abbreviation to identify this category
             when adding references to individual tests. Must be unique across
             all reference categories, and may not be blank.
+        persist (bool, optional): If False this category will only be
+            shown on tests which provide one or more reference items.
+            If True it will be listed on every test regardless of the number
+            of items.
     """
     # Validate title.
     title_stripped = misc.nonempty_string("reference category title", title)
 
     # Validate label.
     label_stripped = misc.nonempty_string("reference category label", label)
-    if label_stripped in state.ref_titles:
+    if label_stripped in categories:
         raise error.UserScriptError(
             f"Duplicate reference label: {label_stripped}",
             f"Create a unique label for {title} references.",
         )
 
-    state.ref_titles[label_stripped] = title_stripped
+    # Validate persist.
+    if not isinstance(persist, bool):
+        raise error.UserScriptError(
+            f"Invalid persist type: {type(persist).__name__}",
+            "persist may only be True or False.",
+        )
+
+    categories[label_stripped] = Category(title_stripped, persist)
 
 
 def get_xref():
@@ -71,7 +99,7 @@ def get_xref():
         ``"sf"`` category would be ``["sf"]["SF42"]``.
     """
     # Initialize all categories with empty dictionaries, i.e., no references.
-    xref = {label: {} for label in state.ref_titles}
+    xref = {label: {} for label in categories}
 
     # Iterate through all Test instances to populate second-level
     # reference dictionaries and test lists.

atform/addtest.py
@@ -9,6 +9,7 @@ from . import id as id_
 from . import label as label_
 from . import misc
 from . import procedure as procedure_
+from . import ref
 from . import state
 from . import term
 
@@ -230,52 +231,79 @@ def validate_refs(refs):
             "References must be a dictionary.",
         )
 
-    valid = [validate_ref_category(*ref) for ref in refs.items()]
+    refs = validate_ref_labels(refs)
+    insert_unlisted_refs(refs)
+    valid = [
+        Reference(lbl, ref.categories[lbl].title, validate_ref_items(lbl, itm))
+        for lbl, itm in refs.items()
+    ]
 
     # Once all categories and items have been validated, sort them
     # them according to the order the categories were defined.
-    categories = list(state.ref_titles.keys())
+    categories = list(ref.categories.keys())
     valid.sort(key=lambda r: categories.index(r.label))
+
+    # Remove empty categories not configured as persist.
+    filtered = filter(lambda r: ref.categories[r.label].persist or r.items, valid)
+
+    return list(filtered)
+
+
+def validate_ref_labels(refs):
+    """Validates all reference category labels."""
+    valid = {}
+
+    for label, items in refs.items():
+        stripped_label = misc.nonempty_string("Reference label", label)
+
+        # Ensure the label has been defined by add_reference_category().
+        try:
+            ref.categories[stripped_label]
+        except KeyError as e:
+            raise error.UserScriptError(
+                f"Invalid reference label: {label}",
+                """Use a reference label that has been previously defined
+                with atform.add_reference_category.""",
+            ) from e
+
+        valid[stripped_label] = items
 
     return valid
 
 
-def validate_ref_category(label, refs):
-    """Validates a single reference category and associated references."""
-    label = misc.nonempty_string("Reference label", label)
+def insert_unlisted_refs(refs):
+    """Adds missing reference categories configured to persist."""
+    for label, cat in ref.categories.items():
+        try:
+            refs[label]
+        except KeyError:
+            if cat.persist:
+                refs[label] = []
 
-    # Ensure the label has been defined by add_reference_category().
-    try:
-        title = state.ref_titles[label]
-    except KeyError as e:
+
+def validate_ref_items(label, raw):
+    """Validate items in a single reference category."""
+    items = []
+
+    if not isinstance(raw, list):
         raise error.UserScriptError(
-            f"Invalid reference label: {label}",
-            """Use a reference label that has been previously defined
-            with atform.add_reference_category.""",
-        ) from e
-
-    # Check the list of references for this category.
-    validated_refs = []
-
-    if not isinstance(refs, list):
-        raise error.UserScriptError(
-            f'Invalid type for "{label}" references: {type(refs).__name__}',
+            f'Invalid type for "{label}" references: {type(raw).__name__}',
             "References for a given category must be contained in a list.",
         )
 
-    for reference in refs:
+    for item in raw:
         try:
-            if not isinstance(reference, str):
+            if not isinstance(item, str):
                 raise error.UserScriptError(
-                    f"Invalid reference list item data type: {type(reference).__name__}",
+                    f"Invalid reference list item data type: {type(item).__name__}",
                     "Items in the list for a reference category must be strings.",
                 )
-            reference = reference.strip()
+            item = item.strip()
 
             # Reject duplicate references.
-            if reference in validated_refs:
+            if item in items:
                 raise error.UserScriptError(
-                    f"Duplicate reference: {reference}",
+                    f"Duplicate reference: {item}",
                     "Ensure all references within a category are unique.",
                 )
 
@@ -284,10 +312,10 @@ def validate_ref_category(label, refs):
             raise
 
         # Ignore blank/empty references.
-        if reference:
-            validated_refs.append(reference)
+        if item:
+            items.append(item)
 
-    return Reference(label, title, validated_refs)
+    return items
 
 
 def validate_string_list(name, lst):

atform/gui/selectref.py
@@ -8,8 +8,8 @@ import tkinter as tk
 from .. import addtest
 from . import buildlist
 from . import common
+from .. import ref
 from . import scrolltree
-from .. import state
 from . import tkwidget
 
 
@@ -72,12 +72,12 @@ class RefList(scrolltree.ScrollTree):  # pylint: disable=too-many-ancestors
     def _populate(self):
         """Adds all reference categories and items to the tree."""
         refs = get_refs()
-        for lbl in state.ref_titles:
+        for lbl in ref.categories:
             # Add the parent category.
             cat_iid = self.insert(
                 "",
                 tk.END,
-                text=state.ref_titles[lbl],
+                text=ref.categories[lbl].title,
             )
             self.tests[cat_iid] = set()
 
@@ -112,12 +112,12 @@ def get_refs():
     Returns a nested dictionary:
     dict[<ref category label>][<ref item>] = set(<test IDs>)
     """
-    refs = {lbl: {} for lbl in state.ref_titles}
+    refs = {lbl: {} for lbl in ref.categories}
 
     for test in addtest.tests.values():
-        for ref in test.references:
-            cat = refs[ref.label]
-            for item in ref.items:
+        for tref in test.references:
+            cat = refs[tref.label]
+            for item in tref.items:
                 try:
                     cat[item].add(test.id)
                 except KeyError:

atform/pdf/refs.py
@@ -41,9 +41,13 @@ def make_references(refs):
 
 def make_row(ref):
     """Creates the table for a single reference category."""
+    if ref.items:
+        items = Paragraph(", ".join(ref.items), stylesheet["Normal"])
+    else:
+        items = Paragraph("No items in this category.", stylesheet["NormalCentered"])
     return [
         Paragraph(ref.title, stylesheet["NormalRight"]),
-        Paragraph(", ".join(ref.items), stylesheet["Normal"]),
+        items,
     ]
 
 

atform/state.py
@@ -20,9 +20,6 @@ in the local namespace, which does not get reinitialized during a reload,
 causing unit test failure.
 """
 
-import collections
-
-
 # Names identifying which fields will be applied to the next test.
 active_fields: set[str] = set()
 
@@ -48,12 +45,6 @@ logo_hash = None  # pylint: disable=invalid-name
 # The current project information set by the most recent call to
 # set_project_info().
 project_info: dict[str, str] = {}
-
-
-# Reference category titles, keyed by label. Stored as an ordered
-# dictionary because the order the categories are created defines
-# the order they are listed in the output documents.
-ref_titles: dict[str, str] = collections.OrderedDict()
 
 
 # Signature titles, in the order they were defined.

tests/gui/test_diff.py
@@ -715,6 +715,17 @@ class References(DiffBase):
         self.generate_diff()
         self.assert_diff(same={(1,)})
 
+    def test_add_empty_category(self):
+        """Confirm adding an empty category is detected as a difference."""
+        atform.add_reference_category("ref", "ref")
+        atform.add_test("title")
+        self.generate_old()
+
+        atform.add_reference_category("ref", "ref", persist=True)
+        atform.add_test("title")
+        self.generate_diff()
+        self.assert_diff(changed={(1,)})
+
     def test_remove_one_category(self):
         """Confirm removing one of many reference categories is detected."""
         atform.add_reference_category("ref1", "ref1")
@@ -751,6 +762,17 @@ class References(DiffBase):
         self.generate_diff()
         self.assert_diff(same={(1,)})
 
+    def test_remove_empty_category(self):
+        """Confirm removing an empty category is detected as a difference."""
+        atform.add_reference_category("ref", "ref", persist=True)
+        atform.add_test("title")
+        self.generate_old()
+
+        atform.add_reference_category("ref", "ref")
+        atform.add_test("title")
+        self.generate_diff()
+        self.assert_diff(changed={(1,)})
+
     def test_change_title(self):
         """Confirm a change to a category's title is detected."""
         atform.add_reference_category("old title", "ref")
@@ -777,6 +799,32 @@ class References(DiffBase):
         self.generate_diff()
         self.assert_diff(changed={(2,)}, same={(1,)})
 
+    def test_add_first_item_to_unused_category(self):
+        """Confirm adding an item to a previouly unused category is detected."""
+        atform.add_reference_category("ref", "ref")
+        atform.add_test("unchanged")
+        atform.add_test("foo")
+        self.generate_old()
+
+        atform.add_reference_category("ref", "ref")
+        atform.add_test("unchanged")
+        atform.add_test("foo", references={"ref": ["a"]})
+        self.generate_diff()
+        self.assert_diff(changed={(2,)}, same={(1,)})
+
+    def test_add_first_item_to_empty_category(self):
+        """Confirm adding an item to a previously empty category is detected."""
+        atform.add_reference_category("ref", "ref", persist=True)
+        atform.add_test("unchanged")
+        atform.add_test("foo")
+        self.generate_old()
+
+        atform.add_reference_category("ref", "ref", persist=True)
+        atform.add_test("unchanged")
+        atform.add_test("foo", references={"ref": ["a"]})
+        self.generate_diff()
+        self.assert_diff(changed={(2,)}, same={(1,)})
+
     def test_remove_item(self):
         """Confirm removing a reference item from an existing category is detected."""
         atform.add_reference_category("ref", "ref")
@@ -787,6 +835,32 @@ class References(DiffBase):
         atform.add_reference_category("ref", "ref")
         atform.add_test("unchanged")
         atform.add_test("foo", references={"ref": ["a"]})
+        self.generate_diff()
+        self.assert_diff(changed={(2,)}, same={(1,)})
+
+    def test_remove_last_item_leaving_unused_category(self):
+        """Confirm removing the last item from a now unused category is detected."""
+        atform.add_reference_category("ref", "ref")
+        atform.add_test("unchanged")
+        atform.add_test("foo", references={"ref": ["a"]})
+        self.generate_old()
+
+        atform.add_reference_category("ref", "ref")
+        atform.add_test("unchanged")
+        atform.add_test("foo")
+        self.generate_diff()
+        self.assert_diff(changed={(2,)}, same={(1,)})
+
+    def test_remove_last_item_leaving_empty_category(self):
+        """Confirm removing the last item from a now empty category is detected."""
+        atform.add_reference_category("ref", "ref", persist=True)
+        atform.add_test("unchanged")
+        atform.add_test("foo", references={"ref": ["a"]})
+        self.generate_old()
+
+        atform.add_reference_category("ref", "ref", persist=True)
+        atform.add_test("unchanged")
+        atform.add_test("foo")
         self.generate_diff()
         self.assert_diff(changed={(2,)}, same={(1,)})
 

tests/test_addtest.py
@@ -338,6 +338,43 @@ class References(unittest.TestCase):
         self.assertEqual(arg, cpy)
 
 
+class EmptyReferences(unittest.TestCase):
+    """Unit tests for references with no items."""
+
+    def setUp(self):
+        utils.reset()
+
+    def test_unlisted(self):
+        """Confirm an unlisted category is excluded."""
+        atform.add_reference_category("ref", "ref")
+        atform.add_test("title")
+        t = utils.get_test_content()
+        self.assertEqual([], t.references)
+
+    def test_empty(self):
+        """Confirm a category with no items is excluded."""
+        atform.add_reference_category("ref", "ref")
+        atform.add_test("title", references={"ref": []})
+        t = utils.get_test_content()
+        self.assertEqual([], t.references)
+
+    def test_unlisted_persist(self):
+        """Confirm an unlisted category configured as persist is included."""
+        atform.add_reference_category("ref", "ref", persist=True)
+        atform.add_test("title")
+        t = utils.get_test_content()
+        self.assertEqual(1, len(t.references))
+        self.assertEqual([], t.references[0].items)
+
+    def test_empty_persist(self):
+        """Confirm a category configured as persist with no items is included."""
+        atform.add_reference_category("ref", "ref", persist=True)
+        atform.add_test("title", references={"ref": []})
+        t = utils.get_test_content()
+        self.assertEqual(1, len(t.references))
+        self.assertEqual([], t.references[0].items)
+
+
 class StringList:
     """Base class for testing a parameter that accepts a list of strings."""
 

tests/test_pdfoutput.py
@@ -123,6 +123,7 @@ class References(Base, unittest.TestCase):
         """Verify table layout with multiple reference categories."""
         atform.add_reference_category("Single Reference", "single")
         atform.add_reference_category("Multiple References", "multi")
+        atform.add_reference_category("Empty", "empty", persist=True)
         atform.add_reference_category("Long List", "long")
         self.make_test(
             references={
@@ -130,7 +131,17 @@ class References(Base, unittest.TestCase):
                 "multi": ["foo", "bar"],
                 # Long enough to require breaking across multiple lines.
                 "long": [str(x) for x in range(50)],
-            }
+            },
+            procedure=[
+                "Verify reference categories are ordered as follows:"
+                + atform.bullet_list(
+                    "Single Reference",
+                    "Multiple Reference",
+                    "Empty",
+                    "Long List",
+                ),
+                "Verify format of Empty reference category.",
+            ],
         )
 
     @nosplit

commit d5ec37d5300e2aac63f55e25f91257fbc495d9fa

Relocate get_xref().
Resolves Pylint cyclic-import(R0401) messages caused by 0f38665.

atform/__init__.py
@@ -34,6 +34,7 @@ from atform.image import (
 
 from atform.meta import (
     get_terms,
+    get_xref,
     list_tests,
 )
 
@@ -45,7 +46,6 @@ from atform.misc import (
 
 from atform.ref import (
     add_reference_category,
-    get_xref,
 )
 
 from atform.sig import (

atform/meta.py
@@ -5,6 +5,7 @@ import collections
 from . import addtest
 from . import error
 from . import id as id_
+from . import ref
 from . import term
 
 
@@ -100,3 +101,36 @@ def get_terms(which="both"):
         ) from e
 
     return format_term_xref(terms)
+
+
+def get_xref():
+    """Builds a cross-reference of tests assigned to each reference.
+
+    For use in the output section of a script, after all tests have
+    been defined.
+
+    .. seealso:: :ref:`xref`
+
+    Returns:
+        dict: A cross-reference between tests and references represented as a
+        nested dictionary. The top-level dictionary is keyed by category labels
+        defined with :py:func:`atform.add_reference_category`; second-level
+        dictionaries are keyed by references in that category, i.e., items
+        passed to the ``references`` argument of :py:func:`atform.add_test`.
+        Final values of the inner dictionary are lists of test identifiers,
+        formatted as strings, assigned to that reference. As an example,
+        the keys yielding a list of all tests assigned ``"SF42"`` in the
+        ``"sf"`` category would be ``["sf"]["SF42"]``.
+    """
+    # Initialize all categories with empty dictionaries, i.e., no references.
+    xref = {label: {} for label in ref.categories}
+
+    # Iterate through all Test instances to populate second-level
+    # reference dictionaries and test lists.
+    for tid in sorted(addtest.tests.keys()):
+        test_id = id_.to_string(tid)
+        for tref in addtest.tests[tid].references:
+            for item in tref.items:
+                xref[tref.label].setdefault(item, []).append(test_id)
+
+    return xref

atform/ref.py
@@ -6,9 +6,7 @@ content passed to the add_test() references parameter.
 
 import collections
 
-from . import addtest
 from . import error
-from . import id as id_
 from . import misc
 
 
@@ -77,36 +75,3 @@ def add_reference_category(title, label, *, persist=False):
         )
 
     categories[label_stripped] = Category(title_stripped, persist)
-
-
-def get_xref():
-    """Builds a cross-reference of tests assigned to each reference.
-
-    For use in the output section of a script, after all tests have
-    been defined.
-
-    .. seealso:: :ref:`xref`
-
-    Returns:
-        dict: A cross-reference between tests and references represented as a
-        nested dictionary. The top-level dictionary is keyed by category labels
-        defined with :py:func:`atform.add_reference_category`; second-level
-        dictionaries are keyed by references in that category, i.e., items
-        passed to the ``references`` argument of :py:func:`atform.add_test`.
-        Final values of the inner dictionary are lists of test identifiers,
-        formatted as strings, assigned to that reference. As an example,
-        the keys yielding a list of all tests assigned ``"SF42"`` in the
-        ``"sf"`` category would be ``["sf"]["SF42"]``.
-    """
-    # Initialize all categories with empty dictionaries, i.e., no references.
-    xref = {label: {} for label in categories}
-
-    # Iterate through all Test instances to populate second-level
-    # reference dictionaries and test lists.
-    for tid in sorted(addtest.tests.keys()):
-        test_id = id_.to_string(tid)
-        for ref in addtest.tests[tid].references:
-            for item in ref.items:
-                xref[ref.label].setdefault(item, []).append(test_id)
-
-    return xref